639657ad3b7fa6320bb7e757c510d7f79c42115b
Myl-W <mylene.wan@gmail.com>
2025-05-02 15:25:27 +0200

    first commit

---
+++ models/users.js
@@ -0,0 +1,14 @@
+// ┬─┬ノ( º _ ºノ)
+// (╯°□°)╯︵ ┻━┻
+const mongoose = require("mongoose");
+
+const usersSchema = mongoose.Schema({
+  firstname: String,
+  username: String,
+  password: String,
+  token: String,
+});
+
+const Users = mongoose.model("users", usersSchema);
+
+module.exports = Users;
